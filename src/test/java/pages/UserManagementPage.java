8292c0bcedecfac777d0d1d0c4c4bda55fbfbc6d
nzmyldz <nzmyldz4@gmail.com>
2022-07-06 02:29:10 +0300

    06.07-02:30

--- src/test/java/pages/UserManagementPage.java
+++ src/test/java/pages/UserManagementPage.java
@@ -1,2 +1,34 @@
-package pages;public class UserManagementPage {
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class UserManagementPage {
+
+    public UserManagementPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//nav/ul/li[last()]")
+    public WebElement lastPageButton;
+
+    @FindBy(css = ".modal-content>div:last-of-type>button:last-of-type")
+    public WebElement confirmDeleteButton;
+
+    @FindBy(xpath = "//div[@class='Toastify__toast-body']")
+    public WebElement deleteSuccessMessage;
+
+    @FindBy(xpath = "(//nav/ul/li)[5]/a")
+    public WebElement beforePageNumber;
+
+    @FindBy(xpath = "(//nav/ul/li)[6]/a")
+    public WebElement actualPageNumber;
+
+
+
+
+
+
 }
