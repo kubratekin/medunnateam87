f15e0d1ab4f36aa75afff38134ad188cfbdd5f7d
fatihkoc1 <102413161+fatihkoc1@users.noreply.github.com>
2022-07-10 14:19:08 +0300

    1007

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",//MUST
         glue = "stepdefinitions",//MUST
-        tags = "@US03",
+        tags = "@US08_TC_0001",
         dryRun =false
 
 )
