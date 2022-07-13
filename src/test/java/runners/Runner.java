2623c9d4c0c9ddadd1d3e549a970929ba82ade72
nzmyldz <nzmyldz4@gmail.com>
2022-07-13 18:54:52 +0300

    13.07-18:55

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",//MUST
         glue = "stepdefinitions",//MUST
-        tags = "@US09_API",
+        tags = "@12-13",
         dryRun = false
 
 )
