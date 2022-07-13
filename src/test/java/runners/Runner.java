3f39bf64226d028f1e62fdf76de8fa0652c00ee3
Yusuf-Cm <102414967+Yusuf-Cm@users.noreply.github.com>
2022-07-14 01:41:37 +0300

    yusuf 14July 01:41

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",//MUST
         glue = "stepdefinitions",//MUST
-        tags = "@12-13",
+        tags = "@team87",
         dryRun = false
 
 )
