c717cca2a5e174dc3c00c64982e1b17565ed4671
Kübra Tekin <kubratekin1611@gmail.com>
2022-07-15 16:56:21 +0300

    15temmuz

--- src/test/java/stepdefinitions/apisteps/US_002_API_Stepdefs.java
+++ src/test/java/stepdefinitions/apisteps/US_002_API_Stepdefs.java
@@ -1,48 +1,49 @@
 package stepdefinitions.apisteps;
 
-import io.cucumber.java.en.*;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Assert;
-import utilities.ApiUtils;
+import utilities.APIUtils;
 
 public class US_002_API_Stepdefs {
 
     @Given("user sends POST request to get Bearer Token")
     public void user_sends_post_request_to_get_bearer_token() {
-        ApiUtils.getSpec();
-        RequestSpecification spec = ApiUtils.getSpec();
+        APIUtils.getSpec();
+        RequestSpecification spec = APIUtils.getSpec();
         spec.pathParams("first", "authenticate");
         String requestBody = "{\"password\":\"vusalgasimov\",\"rememberMe\":true,\"username\":\"vusalgasimov\"}";
-        ApiUtils.setRequestBody(requestBody);
+        APIUtils.setRequestBody(requestBody);
         String relativeURL = "/{first}";
-        ApiUtils.post(relativeURL);
+        APIUtils.post(relativeURL);
     }
     @Then("user verifies that response status code is {int}")
     public void user_verifies_that_response_status_code_is(Integer statusCode) {
-        Response response = ApiUtils.getResponse();
+        Response response = APIUtils.getResponse();
         String res = response.asString();
         response.then().assertThat().statusCode(statusCode);
     }
     @Then("user sends a GET request to get email address")
     public void user_sends_a_get_request_to_get_email_address() {
-        RequestSpecification spec = ApiUtils.getSpec();
+        RequestSpecification spec = APIUtils.getSpec();
         spec.pathParams("first", "users", "second", "notpatient");
-        ApiUtils.addBearerTokenToSpec();
+        APIUtils.addBearerTokenToSpec();
         String relativePath = "/{first}/{second}";
-        ApiUtils.get(relativePath);
+        APIUtils.get(relativePath);
 
     }
     @Then("user verifies that the returned information matches the expected one")
     public void user_verifies_that_the_returned_information_matches_the_expected_one() {
 
-        Response response = ApiUtils.getResponse();
+        Response response = APIUtils.getResponse();
         Assert.assertTrue(response.asString().contains("11mariocosta5@gmail.com"));
     }
 
     @Then("user verifies that the returned information matches the expected one, and is unique")
     public void userVerifiesThatTheReturnedInformationMatchesTheExpectedOneAndIsUnique() {
-        Response response = ApiUtils.getResponse();
+        Response response = APIUtils.getResponse();
         Assert.assertTrue(response.asString().contains("opqrstuvwxyz111"));
         Assert.assertEquals(response.asString().indexOf("opqrstuvwxyz111"),response.asString().lastIndexOf("opqrstuvwxyz111"));
     }
