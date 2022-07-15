8b6c61f47da9edd42a79a8a28db92e035f5f90a7
Kübra Tekin <kubratekin1611@gmail.com>
2022-07-15 17:07:38 +0300

    Merge branch 'master' into kubra
    # Conflicts:
    #	target/default-cucumber-reports.html
    #	target/json-reports/cucumber.json
    #	target/xml-report/cucumber.xml
    #	test-output/PdfReport/ExtentPdf.pdf
    #	test-output/SparkReport/Spark.html

--- src/test/java/stepdefinitions/dbsteps/US07_AppointmentDBStepDefs.java
+++ src/test/java/stepdefinitions/dbsteps/US07_AppointmentDBStepDefs.java
@@ -15,19 +15,19 @@ public class US07_AppointmentDBStepDefs {
 
     List<Object> appointmentListEmail = new ArrayList<>();
 
-    //@Given("user sets a connection with database")
-    //public void user_sets_a_connection_with_database() {
+    @Given("user sets a connection with database")
+    public void user_sets_a_connection_with_database() {
 
-       // DBUtils.createConnection();
-   // }
+        DBUtils.createConnection();
+    }
 
-    //@And("user selects all {string} column data")
-    //public void user_selects_all_column_data(String email) {
-      //  appointmentListEmail = getColumnData("Select * from appointment a, patient p  \n" +
-        //        "where a.patient_id = p.id", email);
-        //System.out.println(appointmentListEmail);
-        //System.out.println("appointmentListId.size() = " + appointmentListEmail.size());
-    //}
+    @And("user selects all {string} column data")
+    public void user_selects_all_column_data(String email) {
+       appointmentListEmail = getColumnData("Select * from appointment a, patient p  \n" +
+                "where a.patient_id = p.id", email);
+        System.out.println(appointmentListEmail);
+        System.out.println("appointmentListId.size() = " + appointmentListEmail.size());
+}
 
     @Then("user validates appointment {string} with the database")
     public void user_validates_appointment_ids_with_the_database(String email) {
