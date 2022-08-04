40ede90526c4bc445a659208f5858826ed1bc859
Feyza Ocakdan <ocakdanfeyza@gmail.com>
2022-08-04 11:19:09 +0300

    feyza04.07

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",//MUST
         glue = "stepdefinitions",//MUST
-        tags = "@US21",
+        tags = "@userManagementU20",
         dryRun = false
 )
 public class Runner {
